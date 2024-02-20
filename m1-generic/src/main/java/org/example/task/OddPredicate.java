4c59f942e161b4232f8792a522087ce00182882a
gozdegunduz <gunduzgzd@gmail.com>
2024-02-20 18:01:38 -0500

    task

---
+++ m1-generic/src/main/java/org/example/task/OddPredicate.java
@@ -0,0 +1,8 @@
+package org.example.task;
+
+public class OddPredicate implements UnaryPredicate<Integer>{
+    @Override
+    public boolean test(Integer obj) {
+        return obj % 2 != 0;
+    }
+}
---
+++ m1-generic/src/main/java/org/example/task/UnaryPredicate.java
@@ -0,0 +1,5 @@
+package org.example.task;
+
+public interface UnaryPredicate<T> {
+public boolean test(T obj);
+}
