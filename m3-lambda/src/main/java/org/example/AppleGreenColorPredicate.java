1874b3249ae9bc8c27a2625e6352952ee05cfedf
gozdegunduz <gunduzgzd@gmail.com>
2024-02-21 14:30:24 -0500

    lambda

---
+++ m3-lambda/src/main/java/org/example/AppleGreenColorPredicate.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class AppleGreenColorPredicate implements ApplePredicate{
+    @Override
+    public boolean test(Apple apple) {
+        return false;
+    }
+}
---
+++ m3-lambda/src/main/java/org/example/ApplePredicate.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface ApplePredicate {
+
+    boolean test (Apple apple);
+}
