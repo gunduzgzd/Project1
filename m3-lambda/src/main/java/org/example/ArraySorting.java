d7ebb5090ca30d272773fcb542035e662d7e9cdf
gozdegunduz <gunduzgzd@gmail.com>
2024-02-21 11:10:58 -0500

    Changes has been made

--- m3-lambda/src/main/java/org/example/ArraySorting.java
+++ m3-lambda/src/main/java/org/example/ArraySorting.java
@@ -5,11 +5,15 @@ public class ArraySorting {
     public static void main(String[] args) {
 
         ArraySorting as = new ArraySorting();
-        as.sort();
+        QuickSort qs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
+
+        as.sort(qs);
+        as.sort(bs);
 
     }
 
-    private void sort(){
-        System.out.println("quick sorting array");
+    private void sort(Sorting sorting){
+        sorting.sort();
     }
 }
--- m3-lambda/src/main/java/org/example/BubbleSort.java
+++ m3-lambda/src/main/java/org/example/BubbleSort.java
@@ -4,6 +4,6 @@ public class BubbleSort implements Sorting{
 
     @Override
     public void sort() {
-        System.out.println("Quick Sorting");
+        System.out.println("Bubble Sorting");
     }
 }
