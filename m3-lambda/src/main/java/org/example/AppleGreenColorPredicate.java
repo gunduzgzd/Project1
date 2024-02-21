2b512821f4cc92439185262682a9efe717c88c86
gozdegunduz <gunduzgzd@gmail.com>
2024-02-21 14:44:26 -0500

    lambda

--- m3-lambda/src/main/java/org/example/AppleGreenColorPredicate.java
+++ m3-lambda/src/main/java/org/example/AppleGreenColorPredicate.java
@@ -3,6 +3,6 @@ package org.example;
 public class AppleGreenColorPredicate implements ApplePredicate{
     @Override
     public boolean test(Apple apple) {
-        return false;
+        return apple.getColor().equals(Color.GREEN);
     }
 }
