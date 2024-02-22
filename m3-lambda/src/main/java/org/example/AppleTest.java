d7d10e789ac127c90143a24aecdca8cd3b753b55
gozdegunduz <gunduzgzd@gmail.com>
2024-02-22 14:17:42 -0500

    lambda

--- m3-lambda/src/main/java/org/example/AppleTest.java
+++ m3-lambda/src/main/java/org/example/AppleTest.java
@@ -14,7 +14,33 @@ public class AppleTest {
         inventory.add(new Apple(200,Color.GREEN));
         inventory.add(new Apple(50,Color.RED));
 
+        List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
+        System.out.println(heavyApple);
+
+        List<Apple> greenApple = filterApples(inventory,new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+
     }
+
+    private static List<Apple> filterApples(List<Apple> inventory,ApplePredicate applePredicate){
+
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory){
+
+            if (applePredicate.test(apple)){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
 
 
 
