ffa04daccc91860b1733ec4769886118e2528642
gozdegunduz <gunduzgzd@gmail.com>
2024-02-26 14:37:18 -0500

    changes has been made

---
+++ m1-generic/src/main/java/org/example/review/Person.java
@@ -0,0 +1,4 @@
+package org.example.review;
+
+public class Person {
+}
---
+++ m1-generic/src/main/java/org/example/review/Teacher.java
@@ -0,0 +1,38 @@
+package org.example.review;
+
+
+public class Teacher extends Person {
+
+    private String name;
+    private int id;
+
+    public Teacher(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+
+    }
+}
