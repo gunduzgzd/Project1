1d607c0139008852314c4faf1ab4da953725bf06
gozdegunduz <gunduzgzd@gmail.com>
2024-02-28 12:03:44 -0500

    ne module

---
+++ m5-comparator_interface/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+        //ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+
+        //descending
+        Collections.sort(list, new MyComparator());
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
