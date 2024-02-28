2f0ab0af2be17a5bfff1e45e07b22e14131d85cb
gozdegunduz <gunduzgzd@gmail.com>
2024-02-28 12:35:29 -0500

    changes has been made

---
+++ m5-comparator_interface/src/main/java/org/example/Apple.java
@@ -0,0 +1,16 @@
+package org.example;
+
+    import lombok.*;
+
+    @Getter
+    @Setter
+    @ToString
+    @AllArgsConstructor
+
+    public class Apple {
+
+        private int weight;
+        private Color color;
+
+    }
+
--- m5-comparator_interface/src/main/java/org/example/Main.java
+++ m5-comparator_interface/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        List<Integer> list = Arrays.asList(10, 0, 15, 5, 20);
         System.out.println(list);
 
         //ascending
@@ -19,9 +19,18 @@ public class Main {
         Collections.sort(list, new MyComparator());
         System.out.println(list);
 
+        Collections.sort(list, (o1, o2) -> (o1 > o2) ? -1 : (o2 > o1) ? 1 : 0);
+        System.out.println(list);
 
+        System.out.println("==================================================");
 
+        //ascending order
+        list.sort((o1,o2)->o1.compareTo(o2));
+        System.out.println(list);
 
+        //descending order
+        list.sort((o2,o1)->o1.compareTo(o2));
+        System.out.println(list);
 
 
 
