2d8960490a7a2570054a9b74af640427dd731f80
gozdegunduz <gunduzgzd@gmail.com>
2024-02-28 20:31:34 -0500

    Stream has been created

---
+++ Stream/src/main/java/org/example/Course.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Course {
+
+    private String courseName;
+    private int courseId;
+}
+
+
