0ef323817681dfecb13f4fd57a681ec44fe85a5d
gozdegunduz <gunduzgzd@gmail.com>
2024-03-05 15:23:12 -0500

    Big Decimal

---
+++ m7-optional-bigdecimal/src/main/java/org/example/OptionalDemo.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2,4,2,10,23);
+
+
+        //.............empty()-isPresent()...............
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        System.out.println(empty.isPresent()); //false
+
+        System.out.println(Optional.of(number).isPresent()); //true
+
+        // System.out.println(empty.get());
+
+        //...................ifPresent.....................
+
+        Optional<Integer> bigNumber = number.stream().filter(x->x>100).findAny();
+        bigNumber.ifPresent(System.out::println);
+
+        //....................get().......................
+
+       // System.out.println(bigNumber.get()); //no value present exception
+
+        //....................orElse.......................
+        System.out.println(bigNumber.orElse(5));  //else is default option case of false
+
+
+
+
+
+
+
+
+    }
+}
